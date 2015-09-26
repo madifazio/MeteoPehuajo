ca485448a6374f36c41eb50aa23ac27e7363bd0a
Marcos Di Fazio <madifazio@gmail.com>
2015-09-26 10:36:56 -0300

    Corregidos warnings de middlewares que quedaron obsoletos.

--- middleware/index.js
+++ middleware/index.js
@@ -8,8 +8,15 @@ module.exports = function(app){
   // diferentes middlewares, antes de cargar la página.
   app.use(morgan('dev'));
   app.use(cookieParser());
-  app.use(session({secret: 'Haciendo una página'}));
-  app.use(bodyParser());
+  app.use(session({
+    secret: 'Haciendo una página',
+    resave: true,
+    saveUninitialized: true
+  }));
+  app.use(bodyParser.urlencoded({
+    extended:true
+  }));
+  app.use(bodyParser.json());
   app.use(function(req, res, next){
     res.locals.session = req.session;
     next(); // busca la siguiente ruta en express
