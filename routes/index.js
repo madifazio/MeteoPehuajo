e26cab3037e7522af92a2329aa90fbf368939433
Marcos Di Fazio <madifazio@gmail.com>
2015-09-28 09:05:47 -0300

    Muestra de datos reales.
    Conexión con openweathermap. utilizando el paquete request de node.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var errors = require('./errors');
+var request = require('request');
+
+
 
 module.exports = function(app){
   // home page
   app.get('/',function(req, res){
-    res.status(200).render('home.jade',{
-      pageTitle:'MeteoPehuajo'
+    request.post(
+      'http://api.openweathermap.org/data/2.5/weather?q=Pehuajo&APPID=c1b55ebf32c85bf328cf2832811d354c&lang=es'
+      , {json: true}
+      , function(err, result, body) {
+          if(err) throw(err);
+          if (!err && result.statusCode === 200) {
+            console.log(body);
+            res.status(200).render('home.jade',{
+              pageTitle:'MeteoPehuajo',
+              datos: body
+            });
+          }
     });
   })
   // error handlers
